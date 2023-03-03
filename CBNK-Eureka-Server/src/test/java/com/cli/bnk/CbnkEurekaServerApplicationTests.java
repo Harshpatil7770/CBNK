66759c60fbbf41d39152f8affde6576bab701a87
Harsh <harshawardhanpatil7770@gmail.com>
2023-03-03 21:25:52 +0530

    Added Eureka Server

---
+++ CBNK-Eureka-Server/src/test/java/com/cli/bnk/CbnkEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cli.bnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbnkEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
